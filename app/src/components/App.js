20548039f9c59f0a3d0dfa79c2a98171e38b85f1
sisi100 <sisii00.swork@gmail.com>
2020-06-04 23:01:46 +0900

    テキストをボタンでインクリメントする機能追加

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: 0};
+  }
+  
+  handleClick(){
+    let newCount = this.state.count + 1
+    this.setState({count:newCount})
+  }
+  
   render() {
     return (
-      <h1>Hello React</h1>
+    	<div>
+        <h1>押してください。</h1>
+        <p>{this.state.count}回押しました！</p>
+        <button onClick={() => {this.handleClick()}}>押す</button>
+      </div>
     );
   }
 }
